a4bd0ee51140644e2e223aee028bf87b3c310b74
autoecoledeschenes <86563710+autoecoledeschenes@users.noreply.github.com>
2021-09-06 12:01:04 +0200

    Add files via upload

--- rebonjour.php
+++ rebonjour.php
@@ -1,15 +1,32 @@
 <?php
-$notes = array(17,18,19,20);
-$i =0;
-while ($i<count($notes))
+
+
+$notes = array(17,18,19,20) ;
+
+// print_r($notes) ;
+
+/*for ($i=0;$i<count($notes);$i++)
 {
     $j=$i+1;
-        echo "la note numero $j est: ".$notes[$i]." <br>";
+    echo "La note n° $j est: ".$notes[$i]."<BR>";
+}*/
 
+/*$j=1;
+for ($i=count($notes);$i>0;$i--)
+{
+    echo "La note n° $j est: ".$notes[$i-1]."<BR>";
+    $j++;
+}*/
+
+/*$i=0;
+while ($i<count($notes))
+{
+    $j=$i+1 ;
+    echo "La note n° $j est: ".$notes[$i]."<BR>";
     $i++;
+}*/
+
+foreach ($notes as $i=>$note){
+    $j=$i+1 ;
+    echo "La note n° $j est: ".$note."<BR>";
 }
-?>
-
-
-
-
